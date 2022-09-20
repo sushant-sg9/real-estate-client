af2039ba750af0bad6bb326c71408a8ccf2a68f1
saikiran-vennam <saikiranvennam64@gmail.com>
2022-09-20 18:37:01 +0530

    Added Login and Register Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/landingPage";
+import Login from './components/login/login.js'
+import Register from './components/register/register';
+import './App.css';
 import PropertiesList from "./components/PropertiesList/propertiesList";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/properties" element={<LandingPage />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
         {/* <Route path="/add-properties" element={< />} /> */}
       </Routes>
     </BrowserRouter>
   );
 }
+
+
 
 export default App;
