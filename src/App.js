2bcfba6ed75bfcb14af150322e606148c7f61ca9
sushant-sg9 <sushantghadge2016@gmail.com>
2022-09-22 11:09:27 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/landingPage";
 import Login from "./components/login/login.js";
 import Register from "./components/register/register";
+import Basic from "./components/forms/Basicinfo";
+import PropertyDetails from "./components/forms/PropertyDetails"
+import GernalInfo from "./components/forms/GernalInfo"
+import Location from "./components/forms/Location"
 import "./App.css";
 
 function App() {
@@ -18,6 +22,16 @@ function App() {
           path="/add-properties"
           element={<LandingPage getProperties={false} />}
         />
+       
+        <Route path="/basic" element={<Basic />} 
+        />
+        <Route path="/propertydetails" element={<PropertyDetails />} 
+        />
+        <Route path="/gernalinfo" element={<GernalInfo />}
+        />
+        <Route path="/location" element={<Location />}
+        />
+        
       </Routes>
     </BrowserRouter>
   );
