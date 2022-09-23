6e1e7fae3c959e9fff7978bf8046c4d1b4cd5dd3
ABHIRAMI P S <psabhirami015@gmail.com>
2022-09-23 13:35:52 +0530

    Update Basicinfo.js

--- src/components/forms/Basicinfo.js
+++ src/components/forms/Basicinfo.js
@@ -29,123 +29,125 @@ const Basic = (props) => {
   return (
     <div className="main">
       <form action="" className="form-basic" onSubmit={handleSubmit}>
-        <div className="form-container">
-          <h4>Property type</h4>
-          <select
-            className="input"
-            name="property"
-            id="Type"
-            required
-            value={
-              props.data.length ? props.data[0].property : basicInfo.property
-            }
-            onChange={(e) =>
-              setBasicInfo([{ ...props.data, property: e.target.value }])
-            }
-          >
-            <option value="" disabled selected hidden>
-              Select Property type
-            </option>
-            <option value="Flat">Flat</option>
-            <option value="House">House</option>
-            <option value="Plot">Plot</option>
-          </select>
-          <h4>Price</h4>
-          <input
-            type="text"
-            name="price"
-            id="Price"
-            value={props.data.length ? props.data[0].price : basicInfo.price}
-            onChange={(e) =>
-              setBasicInfo([{ ...props.data, price: e.target.value }])
-            }
-            placeholder="Example: 10000"
-            className="input"
-          />
-          <h4>Property age</h4>
-          <input
-            type="text"
-            id="age"
-            name="age"
-            placeholder="Select Property Age"
-            className="input"
-            value={props.data.length ? props.data[0].age : basicInfo.age}
-            onChange={(e) =>
-              setBasicInfo([{ ...props.data, age: e.target.value }])
-            }
-          />
-          <h4>Property Description</h4>
-          <input
-            type="text"
-            id="desc"
-            placeholder=""
-            name="description"
-            className="input"
-            value={
-              props.data.length
-                ? props.data[0].description
-                : basicInfo.description
-            }
-            onChange={(e) =>
-              setBasicInfo([{ ...props.data, description: e.target.value }])
-            }
-          />
-        </div>
-        <div className="form-container1">
-          <h4>Negotiable</h4>
-          <input
-            type="text"
-            id="nego"
-            name="negotiable"
-            placeholder="name"
-            className="input"
-            value={
-              props.data.length
-                ? props.data[0].negotiable
-                : basicInfo.negotiable
-            }
-            onChange={(e) =>
-              setBasicInfo([{ ...props.data, negotiable: e.target.value }])
-            }
-          />
-          <h4>Ownership</h4>
-          <input
-            type="text"
-            id="own"
-            name="owner"
-            placeholder="name"
-            className="input"
-            value={props.data.length ? props.data[0].owner : basicInfo.owner}
-            onChange={(e) =>
-              setBasicInfo([{ ...props.data, owner: e.target.value }])
-            }
-          />
-          <h4>Property Approved</h4>
-          <input
-            type="text"
-            id="approv"
-            name="approval"
-            placeholder="name"
-            className="input"
-            value={
-              props.data.length ? props.data[0].approval : basicInfo.approval
-            }
-            onChange={(e) =>
-              setBasicInfo([{ ...props.data, approval: e.target.value }])
-            }
-          />
-          <h4>Bank Loan</h4>
-          <input
-            type="text"
-            id="loan"
-            name="loan"
-            value={props.data.length ? props.data[0].loan : basicInfo.loan}
-            onChange={(e) =>
-              setBasicInfo([{ ...props.data, loan: e.target.value }])
-            }
-            placeholder="name"
-            className="input"
-          />
+        <div className="form-data1">
+          <div className="form-container">
+            <h4>Property type</h4>
+            <select
+              className="input"
+              name="property"
+              id="Type"
+              required
+              value={
+                props.data.length ? props.data[0].property : basicInfo.property
+              }
+              onChange={(e) =>
+                setBasicInfo([{ ...props.data, property: e.target.value }])
+              }
+            >
+              <option value="" disabled selected hidden>
+                Select Property type
+              </option>
+              <option value="Flat">Flat</option>
+              <option value="House">House</option>
+              <option value="Plot">Plot</option>
+            </select>
+            <h4>Price</h4>
+            <input
+              type="text"
+              name="price"
+              id="Price"
+              value={props.data.length ? props.data[0].price : basicInfo.price}
+              onChange={(e) =>
+                setBasicInfo([{ ...props.data, price: e.target.value }])
+              }
+              placeholder="Example: 10000"
+              className="input"
+            />
+            <h4>Property age</h4>
+            <input
+              type="text"
+              id="age"
+              name="age"
+              placeholder="Select Property Age"
+              className="input"
+              value={props.data.length ? props.data[0].age : basicInfo.age}
+              onChange={(e) =>
+                setBasicInfo([{ ...props.data, age: e.target.value }])
+              }
+            />
+            <h4>Property Description</h4>
+            <input
+              type="text"
+              id="desc"
+              placeholder=""
+              name="description"
+              className="input"
+              value={
+                props.data.length
+                  ? props.data[0].description
+                  : basicInfo.description
+              }
+              onChange={(e) =>
+                setBasicInfo([{ ...props.data, description: e.target.value }])
+              }
+            />
+          </div>
+          <div className="form-container1">
+            <h4>Negotiable</h4>
+            <input
+              type="text"
+              id="nego"
+              name="negotiable"
+              placeholder="name"
+              className="input"
+              value={
+                props.data.length
+                  ? props.data[0].negotiable
+                  : basicInfo.negotiable
+              }
+              onChange={(e) =>
+                setBasicInfo([{ ...props.data, negotiable: e.target.value }])
+              }
+            />
+            <h4>Ownership</h4>
+            <input
+              type="text"
+              id="own"
+              name="owner"
+              placeholder="name"
+              className="input"
+              value={props.data.length ? props.data[0].owner : basicInfo.owner}
+              onChange={(e) =>
+                setBasicInfo([{ ...props.data, owner: e.target.value }])
+              }
+            />
+            <h4>Property Approved</h4>
+            <input
+              type="text"
+              id="approv"
+              name="approval"
+              placeholder="name"
+              className="input"
+              value={
+                props.data.length ? props.data[0].approval : basicInfo.approval
+              }
+              onChange={(e) =>
+                setBasicInfo([{ ...props.data, approval: e.target.value }])
+              }
+            />
+            <h4>Bank Loan</h4>
+            <input
+              type="text"
+              id="loan"
+              name="loan"
+              value={props.data.length ? props.data[0].loan : basicInfo.loan}
+              onChange={(e) =>
+                setBasicInfo([{ ...props.data, loan: e.target.value }])
+              }
+              placeholder="name"
+              className="input"
+            />
+          </div>
         </div>
         <div className="button">
           <Link to="/property">
